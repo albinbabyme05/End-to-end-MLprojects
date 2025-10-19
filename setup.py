477f3d13635707ab946c4dd282516ef03ddbc2a7
Albin Baby <albinbabayme05@gmail.com>
2025-10-19 10:59:43 +0200

    Set up

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+from typing import List
+
+E_DOT = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if E_DOT in requirements:
+            requirements.remove(E_DOT)
+    
+    return requirements
+        
+    
+setup(
+    name = 'MLPROJECT1',
+    version = '0.0.1',
+    author='Albin',
+    author_email='albinbaby150@gmail.com',
+    packages= find_packages(),
+    install_requires = get_requirements('./requirement.txt')
+)
